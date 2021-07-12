fbe26ef84cfeac50bbd884f2509011020622331a
nuts-rice <msis44443@gmail.com>
2021-07-12 14:49:46 -0400

    Update jsonic-parse-test.rkt

--- racket/jsonic/jsonic-parse-test.rkt
+++ racket/jsonic/jsonic-parse-test.rkt
@@ -1,4 +1,8 @@
 #lang br
 (require jsonic/parser jsonic/tokenizer brag/support)
-(parse-to-datum (apply-tokenizer-maker make-tokenizer "// line comment\n"))
-(parse-to-datum (apply-tokenizer-maker make-tokenizer "@$ 42 $@"))
+(parse-to-datum (apply-tokenizer-maker make-tokenizer #<<MISHKA
+"Foo"
+//comment
+@$ 42 $@
+MISHKA
+                                       ))

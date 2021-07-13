ea45150a1060345047b371be0a15bc9c6115af83
nuts-rice <msis44443@gmail.com>
2021-07-13 09:06:40 -0400

    unit testing

--- racket/jsonic/reader.rkt
+++ racket/jsonic/reader.rkt
@@ -1,7 +1,10 @@
 #lang br/quicklang
-(require "tokenizer.rkt" "parser.rkt")
+(require "tokenizer.rkt" "parser.rkt" racket/contract)
 
 (define (read-syntax path port)
-  (datum->syntax #f `(module jsonic-mod jsonic/expander
-                       ,(parse path(make-tokenizer port)))))
-(provide read-syntax)
+  (define parse-tree (parse path (make-tokenizer port)))
+  (define module-datum `(module jsonic-module jsonic/expander
+                          ,parse-tree))
+  (datum->syntax #f module-datum))
+(provide (contract-out
+          [read-syntax (any/c input-port? . -> . syntax?)]))
